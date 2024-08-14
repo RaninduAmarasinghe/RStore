ba89d5c98e884e0b2c25db41dc6ccb8c4cef08d4
RaninduAmarasinghe <raninduamarasinghe@gmail.com>
2024-08-14 20:38:20 +0530

    first commit

---
+++ src/main/java/com/ranindu/rstore/RstoreApplication.java
@@ -0,0 +1,13 @@
+package com.ranindu.rstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RstoreApplication.class, args);
+	}
+
+}
