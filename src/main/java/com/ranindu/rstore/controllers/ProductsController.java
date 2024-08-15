0e73fa5076e941c4801ba6412b11815b1423953c
RaninduAmarasinghe <raninduamarasinghe@gmail.com>
2024-08-15 20:08:55 +0530

    add product dto class

--- src/main/java/com/ranindu/rstore/controllers/ProductsController.java
+++ src/main/java/com/ranindu/rstore/controllers/ProductsController.java
@@ -3,6 +3,7 @@ package com.ranindu.rstore.controllers;
 import com.ranindu.rstore.models.Product;
 import com.ranindu.rstore.services.ProductsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,7 +22,7 @@ public class ProductsController {
 
     @GetMapping({"","/"})
     public String showProductList(Model model) {
-        List<Product> products = repo.findAll();
+        List<Product> products = repo.findAll(Sort.by(Sort.Direction.DESC,"id"));
         model.addAttribute("products", products);
 
         return "products/index";
---
+++ src/main/java/com/ranindu/rstore/models/ProductDto.java
@@ -0,0 +1,28 @@
+package com.ranindu.rstore.models;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import org.springframework.web.multipart.MultipartFile;
+
+public class ProductDto {
+
+    @NotEmpty(message = "The name is required")
+    private String name;
+
+    @NotEmpty(message = "The brand is required")
+    private String brand;
+
+    @NotEmpty(message = "The category is required")
+    private String category;
+
+    @Min(0)
+    private double price;
+
+    @Size(min = 10, message = "The description should be at least 10 characters")
+    @Size(max = 2000,message = "The description cannot exceed 2000 charactors")
+
+    private String description;
+
+    private MultipartFile imageFile;
+}
