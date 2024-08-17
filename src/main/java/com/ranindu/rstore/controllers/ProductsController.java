93446cb3c09ee7f8e515bac316104afda968bd76
RaninduAmarasinghe <raninduamarasinghe@gmail.com>
2024-08-17 13:55:28 +0530

    create product and show produt page

--- src/main/java/com/ranindu/rstore/controllers/ProductsController.java
+++ src/main/java/com/ranindu/rstore/controllers/ProductsController.java
@@ -14,7 +14,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.InputStream;
+import java.nio.file.*;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -53,6 +59,41 @@ public class ProductsController {
 if(result.hasErrors()){
     return "products/CreateProduct";
 }
+
+    MultipartFile image =productDto.getImageFile();
+    Date createdAt = new Date();
+    String storageFileName = createdAt.getTime()+"_"+image.getOriginalFilename();
+
+    try {
+        String uploadDir = "public/images/";
+        Path uploadPath = Paths.get(uploadDir);
+
+        if (!Files.exists(uploadPath)) {
+            Files.createDirectories(uploadPath);
+        }
+
+        try (InputStream inputStream = image.getInputStream()) {
+            Files.copy(inputStream, Paths.get(uploadDir + storageFileName),
+                    StandardCopyOption.REPLACE_EXISTING);
+        }
+
+    } catch (Exception ex){
+        System.out.println("Exception:" + ex.getMessage());
+    }
+
+    Product product = new Product();
+    product.setName(productDto.getName());
+    product.setBrand(productDto.getBrand());
+    product.setCategory(productDto.getCategory());
+    product.setPrice(productDto.getPrice());
+    product.setDescription(productDto.getDescription());
+    product.setCreatedAt(createdAt);
+    product.setImageFileName(storageFileName);
+
+    repo.save(product);
+
+
+
 
         return "redirect:/products";
 }
