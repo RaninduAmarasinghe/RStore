5dcbf77ac99d60e6b09e57b3f8cf2b09a316a36f
RaninduAmarasinghe <raninduamarasinghe@gmail.com>
2024-08-17 13:15:53 +0530

    create image validation

--- src/main/java/com/ranindu/rstore/controllers/ProductsController.java
+++ src/main/java/com/ranindu/rstore/controllers/ProductsController.java
@@ -3,11 +3,16 @@ package com.ranindu.rstore.controllers;
 import com.ranindu.rstore.models.Product;
 import com.ranindu.rstore.models.ProductDto;
 import com.ranindu.rstore.services.ProductsRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -37,5 +42,19 @@ public class ProductsController {
         return "products/CreateProduct";
     }
 
+@PostMapping("/create")
+
+    public String createProduct(@Valid @ModelAttribute ProductDto productDto, BindingResult result)
+{
+
+    if(productDto.getImageFile().isEmpty()){
+        result.addError(new FieldError("productDto", "imageFile", "Image file is required"));
+    }
+if(result.hasErrors()){
+    return "products/CreateProduct";
+}
+
+        return "redirect:/products";
+}
 
 }
